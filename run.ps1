9867618edc1e13b719b1984dc09faecc4a88779d
hendahan <hendahan110@gmail.com>
2021-06-12 12:46:53 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,13 +1,13 @@
 # start run from here 
 
 # Connect to Azure
-Connect-AzAccount -TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47
+#Connect-AzAccount -TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47
 
 #create 100 files to directory:
-New-Item -Path blobsFolder -ItemType Directory
-for ($num = 1 ; $num -le 100 ; $num++){
-	New-Item -Path "blobsFolder\$num.jpg" -ItemType File
-}
+#New-Item -Path blobsFolder -ItemType Directory
+#for ($num = 1 ; $num -le 100 ; $num++){
+#	New-Item -Path "blobsFolder\$num.jpg" -ItemType File
+#}
 
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
@@ -23,5 +23,5 @@ New-AzResourceGroupDeployment `
 
 
  # remove 100 file from directory: 
- Remove-Item blobsFolder
+ #Remove-Item blobsFolder
  
