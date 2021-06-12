41e32e7e3f1c3b4c0b5ee8741b3ec7e8eff50550
hendahan <hendahan110@gmail.com>
2021-06-12 13:25:37 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -8,18 +8,21 @@
 #for ($num = 1 ; $num -le 100 ; $num++){
 #	New-Item -Path "blobsFolder\$num.jpg" -ItemType File
 #}
+$resourceGroupName = "arm-Home-Work"
+$location = "West Europe" 
 
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
-	-TemplateFile storage.json `
     -TemplateFile vm.json `
 	-TemplateParameterFile vm.parameters.json
-  
+
 
  (Get-AzVm -ResourceGroupName $resourceGroupName).name
 
-
+ New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile storage.json 
 
 
  # remove 100 file from directory: 
